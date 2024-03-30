93b7c4b2b2a790ef5a73177d45f84e0e13542b5b
Pankaj Mishra <pankajmishra84454@gmail.com>
2024-03-30 12:06:48 +0530

    all files commit

---
+++ src/data/education.js
@@ -0,0 +1,28 @@
+function article_1() {
+	return {
+		date: "2021 - 2025",
+		title: "B.Tech in Computer Science and Engineering",
+		description: "From Graphic Era Hill University, Dehradun, Uttarakhand.",
+	};
+}
+
+function article_2() {
+	return {
+		date: "2020 - 2021",
+		title: "Intermediate",
+		description:
+			"From Shri Guru Ram Rai Public School Bombay Bagh, Dehradun, Uttarakhand",
+	};
+}
+function article_3() {
+	return {
+		date: "2018 - 2019",
+		title: "High School",
+		description:
+			"From Shri Guru Ram Rai Public School Bombay Bagh, Dehradun, Uttarakhand",
+	};
+}
+
+const myArticles = [article_1, article_2, article_3];
+
+export default myArticles;
---
+++ src/data/user.js
@@ -0,0 +1,75 @@
+const INFO = {
+	main: {
+		title: "Portfolio",
+		name: "Pankaj Mishra",
+		email: "pankajmishra31@outlook.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer.",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Pankaj Mishra.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "Education Details",
+		description:
+			"",
+	},
+
+	projects: [
+		{
+			title: "Flowchart Generator",
+			description:
+				"Developed using Generative AI, it generated flowchart for users query",
+			logo: "../openai.svg",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Human Pose Estimation",
+			description:
+				"Uses Openpose and python to detect human pose and posture.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Blog Website",
+			description:
+				"Using React Js for frontend  and Appwrite for backend service",
+			logo: "../react_logo.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Library Management System",
+			description:
+				"Using html,css, javascript for frontend, nodejs for server side and mongodb as database service",
+			logo: "../html_logo.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
